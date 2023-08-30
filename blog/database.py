c37c0b00cd4a3e263dd9e00d6eda166a2299c33a
Rick <48156842+Rick108@users.noreply.github.com>
2023-08-30 16:11:57 +0530

    Update database.py

--- blog/database.py
+++ blog/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
+SQLALCHEMY_DATABASE_URL = 'mysql://Rick:pswd1@192.168.0.151/FastAPI_database'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False})
 
